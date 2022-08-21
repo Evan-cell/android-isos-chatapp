adc8089782a0a41dcb2d4ff354e0150c88540741
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-21 20:54:43 +0300

    text

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{fontSize: 30}}>mwema AIC CHANGAMWE SOLOOOO!</Text>
+      <Text style={{fontSize: 30}}>mwema AIC CHANGAMWE SOLOOOO!</Text>
       <StatusBar style="auto" />
     </View>
   );
